5967d180c2920e6e61d9fc37054e53c3b990ff90
samatrhea <s.gerene@rheagroup.com>
2019-04-29 19:04:58 +0200

    [Add] Visual Studio solution

---
+++ ECSS-E-TM-10-25A-Annex-B-Validator.Tests/Dummy.cs
@@ -0,0 +1,27 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="Dummy.cs" company="RHEA System S.A.">
+//   Copyright (c) 2019 RHEA System S.A.
+//
+//   This file is part of ECSS-E-TM-10-25A Annex B Validator
+//
+//   The ECSS-E-TM-10-25A Annex B Validator is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//   
+//   ECSS-E-TM-10-25A Annex B Validator is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with Foobar.  If not, see<http://www.gnu.org/licenses/>.
+// </copyright>
+// -------------------------------------------------------------------------------------------------
+
+namespace com.rheagroup.validator.tests
+{
+    public class Dummy
+    {
+    }
+}
---
+++ ECSS-E-TM-10-25A-Annex-B-Validator/Program.cs
@@ -0,0 +1,33 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="RHEA System S.A.">
+//   Copyright (c) 2019 RHEA System S.A.
+//
+//   This file is part of ECSS-E-TM-10-25A Annex B Validator
+//
+//   The ECSS-E-TM-10-25A Annex B Validator is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//   
+//   ECSS-E-TM-10-25A Annex B Validator is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with Foobar.  If not, see<http://www.gnu.org/licenses/>.
+// </copyright>
+// -------------------------------------------------------------------------------------------------
+
+namespace com.rheagroup.validator
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
