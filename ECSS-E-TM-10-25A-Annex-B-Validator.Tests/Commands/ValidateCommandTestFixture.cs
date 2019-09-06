2cd5656efe19b16a5d7595fcf0a947241cafa4e9
samatrhea <s.gerene@rheagroup.com>
2019-09-07 00:16:05 +0200

    [Add] test for validateCommand

---
+++ ECSS-E-TM-10-25A-Annex-B-Validator.Tests/Commands/ValidateCommandTestFixture.cs
@@ -0,0 +1,85 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="ValidateCommandTestFixture.cs" company="RHEA System S.A.">
+//   Copyright (c) 2019 RHEA System S.A.
+//
+//   This file is part of ECSS-E-TM-10-25A Annex B Validator
+//
+//   The ECSS-E-TM-10-25A Annex B Validator is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//   
+//   ECSS-E-TM-10-25A Annex B Validator is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with ECSS-E-TM-10-25A Annex B Validator. If not, see<http://www.gnu.org/licenses/>.
+// </copyright>
+// -------------------------------------------------------------------------------------------------
+
+using System.IO;
+using System.Runtime.CompilerServices;
+using CDP4Common.MetaInfo;
+
+namespace com.rheagroup.validator.tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using com.rheagroup.validator.Commands;
+    using com.rheagroup.validator.Reporting;
+    using CDP4Rules;
+    using Moq;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Suite of tests for the <see cref="ValidateCommand"/> class
+    /// </summary>
+    [TestFixture]
+    public class ValidateCommandTestFixture
+    {
+        private Mock<IFolderStructureValidator> folderStructureValidator;
+
+        private ISiteReferenceDataLibraryReader siteReferenceDataLibraryReader;
+
+        private Mock<IRuleCheckerEngine> ruleCheckerEngine;
+
+        private Mock<IReportingService> reportingService;
+
+        private ValidateCommand validateCommand;
+
+        private string source;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.source = System.IO.Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "annex-c3-model");
+            var directoryInfo = new DirectoryInfo(this.source);
+
+            this.folderStructureValidator = new Mock<IFolderStructureValidator>();
+            this.folderStructureValidator.Setup(x => x.Validate(It.IsAny<string>())).Returns(directoryInfo);
+
+            var metaDataProvider = StaticMetadataProvider.GetMetaDataProvider;
+            this.siteReferenceDataLibraryReader = new SiteReferenceDataLibraryReader(this.folderStructureValidator.Object, metaDataProvider);
+            
+            this.ruleCheckerEngine = new Mock<IRuleCheckerEngine>();
+            this.reportingService = new Mock<IReportingService>();
+            
+            this.validateCommand = new ValidateCommand(
+                this.folderStructureValidator.Object, 
+                this.siteReferenceDataLibraryReader,
+                this.ruleCheckerEngine.Object,
+                this.reportingService.Object);
+
+            this.validateCommand.Source = this.source;
+        }
+
+        [Test]
+        public async Task Verify_that_the_ValidateCommand_can_Execute()
+        {
+            Assert.DoesNotThrowAsync(async () => await this.validateCommand.Execute()); 
+        }
+    }
+}
